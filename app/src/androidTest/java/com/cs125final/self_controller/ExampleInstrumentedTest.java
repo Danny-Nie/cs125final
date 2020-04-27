5205a671b997890faee32296ad2c45a706b61041
Danny-Nie <xn2@illinois.edu>
2020-04-26 21:26:38 -0500

    Test1

--- app/src/androidTest/java/com/cs125final/self_controller/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/cs125final/self_controller/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.cs125final.self_controller;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
--- app/src/main/java/com/cs125final/self_controller/MainActivity.java
+++ app/src/main/java/com/cs125final/self_controller/MainActivity.java
@@ -1,6 +1,7 @@
 package com.cs125final.self_controller;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("This is a test commit message");
     }
 }
